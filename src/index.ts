6b07a369aae466bb233b5cc77eaf3eb04ee8e33e
extr0py <extr0py@extropygames.com>
2016-03-27 09:51:48 -0700

    Delete previous strategy and use a tsserver based strategy for definitions

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,50 @@
+/// <reference path="typings/main/ambient/node/node.d.ts" />
 import path = require("path");
 import fs = require("fs");
 
 declare var vim;
+declare var log;
+
 import * as ts from "typescript";
 
-import * as tspm from "./TypeScriptProjectManager"
+import * as tshost from "./TypeScriptServerHost"
 
-var autoCompleter = {
-    getCompletions: (args) => {
-        console.log(JSON.stringify(args));
-            var project = tspm.getProjectFromFile(args.currentBuffer);
+var host = new tshost.TypeScriptServerHost();
 
-            if(args.tempFile) {
-                console.log("Temp file specified");
+// var autoCompleter = {
+//     getCompletions: (args) => {
+//         log.verbose(JSON.stringify(args));
+//             var project = tspm.getProjectFromFile(args.currentBuffer);
 
-                project.updateFile(args.currentBuffer, fs.readFileSync(args.tempFile, "utf8"));
-            }
-        var completions = project.getCompletions(args.currentBuffer, null, args.byte);
-        return completions;
-    }
-};
+//             if(args.tempFile) {
+//                 log.verbose("Temp file specified");
 
-vim.addOmniCompleter(autoCompleter);
+//                 project.updateFile(args.currentBuffer, fs.readFileSync(args.tempFile, "utf8"));
+//             }
+//         var completions = project.getCompletions(args.currentBuffer, null, args.byte);
+//         return completions;
+//     }
+// };
+
+// vim.addOmniCompleter(autoCompleter);
 
 vim.addCommand("TSDefinition", (args) => {
-
-    var project = tspm.getProjectFromFile(args.currentBuffer);
-    project.updateFile(args.currentBuffer, fs.readFileSync(args.currentBuffer, "utf8"));
-
-    var def = (project.getDefinition(args.currentBuffer, args.byte));
-    console.log(def);
-    
-    if(def) {
-        vim.exec(":e " + def.fileName + " | " + "goto " + def.byteOffset);
-    }
-
-    // var host = new TypeScriptLanguageServiceHost();
-    // var services = ts.createLanguageService(host, ts.createDocumentRegistry());
-
-    // host.addFile("lib.d.ts", fs.readFileSync(libPath, "utf8"));
-    // host.addFile(args.currentBuffer, fs.readFileSync(args.currentBuffer, "utf8"));
-
-    // var definition = services.getDefinitionAtPosition(args.currentBuffer, args.byte);
-
-    // if(definition) {
-    //     console.log(JSON.stringify(definition));
-    // } else {
-    //     vim.echo("TS: Unable to locate definition.");
-    // }
-
-    // var completions = services.getCompletionsAtPosition(args.currentBuffer, args.byte);
-
-    // console.log(JSON.stringify(completions));
-
+    host.openFile(args.currentBuffer);
+    host.getTypeDefinition(args.currentBuffer, parseInt(args.line), parseInt(args.col)).then((val: any) => {
+        vim.exec(":e " + val.file + " | :norm " + val.start.line + "G" + val.start.offset + "|");
+    }, (err) => {
+        vim.echo("Error: " + err);
+    });
 });
 
-vim.addCommand("TSGetCompletions", (args) => {
-    console.log("TSGETCOMPLETIONS!");
+// vim.addCommand("TSGetCompletions", (args) => {
+//     console.log("TSGETCOMPLETIONS!");
     
-    var project = tspm.getProjectFromFile(args.currentBuffer);
+//     var project = tspm.getProjectFromFile(args.currentBuffer);
 
-    var completions = project.getCompletions(args.currentBuffer, null, args.byte);
-    console.log("Completion count: " + completions.length);
-});
+//     var completions = project.getCompletions(args.currentBuffer, null, args.byte);
+//     if(completions)
+//         console.log("Completion count: " + completions.length);
+//     else
+//         console.log("No completions");
+// });

c2917bd65ffd1532e66b84827a9f4d5282f16dda
extr0py <extr0py@extropygames.com>
2016-05-11 09:48:42 -0700

    Fix up typings

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require("gulp");
 var merge = require("merge2");
 var ts = require("gulp-typescript");
+var gulpTypings = require("gulp-typings");
 
-var srcProject = ts.createProject("src\\tsconfig.json");
-var testProject = ts.createProject("test\\tsconfig.json");
 
 gulp.task("build:src", function() {
+    var srcProject = ts.createProject("src\\tsconfig.json");
     var tsResult = srcProject.src() 
         .pipe(ts(srcProject));
 
@@ -13,13 +13,26 @@ gulp.task("build:src", function() {
 });
 
 gulp.task("build:test", function() {
+    var testProject = ts.createProject("test\\tsconfig.json");
     var tsResult = testProject.src() 
         .pipe(ts(testProject));
 
     return tsResult.js.pipe(gulp.dest("test"));
 });
+
+gulp.task("typings:src", function() {
+    return gulp.src("src/typings.json")
+            .pipe(gulpTypings());
+});
+
+gulp.task("typings:test", function() {
+    return gulp.src("test/typings.json")
+            .pipe(gulpTypings());
+});
+
 gulp.watch("src\\**\\*.ts", gulp.parallel("build:src"));
 gulp.watch("test\\**\\*.ts", gulp.parallel("build:test"));
 
+gulp.task("typings", gulp.series("typings:src", "typings:test"));
 gulp.task("build", gulp.series("build:src", "build:test"));
 gulp.task("default", gulp.series("build"));

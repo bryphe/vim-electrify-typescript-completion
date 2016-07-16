15b94dd4010422424f8c2f294d2c8f3b870092ff
extr0py <extr0py@extropygames.com>
2016-07-16 13:54:53 -0700

    Remove some extra code

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,6 @@ vim.on("CursorMovedI", (args) => {
 });
 
 function showQuickInfo(args) {
-
     host.getQuickInfo(args.currentBuffer, parseInt(args.line), parseInt(args.col)).then((val: any) => {
         console.log("Quick info: " + JSON.stringify(val));
         var outputString = val.displayString;
@@ -50,20 +49,6 @@ function showQuickInfo(args) {
         vim.echo(outputString);
     });
 }
-
-
-vim.addCommand("TSSaveDebug", (args) => {
-    host.saveTo(args.currentBuffer, "C:/test-file.txt");
-});
-
-vim.addCommand("TSSuperDerp", (args) => {
-    host.updateFile(args.currentBuffer, "DERP");
-});
-
-vim.addCommand("TSSuperUpdate", (args) => {
-
-    host.updateFile(args.currentBuffer, cachedContents);
-});
 
 vim.addCommand("TSDefinition", (args) => {
     host.getTypeDefinition(args.currentBuffer, parseInt(args.line), parseInt(args.col)).then((val: any) => {
@@ -79,71 +64,11 @@ vim.addCommand("TSDefinition", (args) => {
     });
 });
 
-vim.addCommand("TSCompletions", (args) => {
-    host.getCompletions(args.currentBuffer, parseInt(args.line), parseInt(args.col)).then((val: any) => {
-        console.log("Completions: " + JSON.stringify(val));
-        // vim.exec(":e " + val.file + " | :norm " + val.start.line + "G" + val.start.offset + "| | zz");
-    }, (err) => {
-        vim.echo("Error: " + err);
-    });
-});
-
-vim.addCommand("TSCompletionDetails", (args) => {
-    host.getCompletions(args.currentBuffer, parseInt(args.line), parseInt(args.col))
-        .then((val: any) => {
-            var names = val.map((v) => v.name);
-            console.log("Names: " + JSON.stringify(names));
-
-            host.getCompletionDetails(args.currentBuffer, parseInt(args.line), parseInt(args.col), names)
-            .then((result => {
-                console.log("Completoins with details: " + JSON.stringify(result));
-            }))
-        });
-})
-
-vim.addCommand("TSQuickInfo", (args) => {
-    host.getQuickInfo(args.currentBuffer, parseInt(args.line), parseInt(args.col)).then((val: any) => {
-        console.log("Quick info: " + JSON.stringify(val));
-        vim.echo(val.displayString);
-        // vim.exec(":e " + val.file + " | :norm " + val.start.line + "G" + val.start.offset + "| | zz");
-    }, (err) => {
-        vim.echo("Error: " + err);
-    });
-});
-
-vim.addCommand("TSSignatureHelp", (args) => {
-    host.getSignatureHelp(args.currentBuffer, parseInt(args.line), parseInt(args.col)).then((val: any) => {
-        console.log("Signature help" + JSON.stringify(val));
-        // vim.exec(":e " + val.file + " | :norm " + val.start.line + "G" + val.start.offset + "| | zz");
-    }, (err) => {
-        vim.echo("Error: " + err);
-    });
-});
-
 vim.addCommand("TSErrors", (args) => {
     host.getErrors(args.currentBuffer).then((val: any) => {
         // vim.exec(":e " + val.file + " | :norm " + val.start.line + "G" + val.start.offset + "| | zz");
     }, (err) => {
         vim.echo("Error: " + err);
-    });
-});
-
-vim.addCommand("TSDerp", (args) => {
-    host.getSignatureHelp(args.currentBuffer, parseInt(args.line), parseInt(args.col)).then((val: any) => {
-        // vim.exec(":e " + val.file + " | :norm " + val.start.line + "G" + val.start.offset + "| | zz");
-    }, (err) => {
-        vim.echo("Error: " + err);
-    });
-});
-
-vim.addCommand("TSNavigationBarItems", (args) => {
-    host._makeTssRequest<void>("navbar", {
-        file: args.currentBuffer
-    }).then((val: any) => {
-        console.log(JSON.stringify(val));
-        console.log(JSON.stringify(val));
-    }, (err) => {
-        console.log("Error:" + err);
     });
 });
 
@@ -170,23 +95,3 @@ function updateSyntaxHighlighting(file) {
         console.error(err);
     });
 }
-
-vim.addCommand("TSProcessID", (args) => {
-    vim.echo(host.pid);
-});
-
-vim.addCommand("TSGetHostProcessID", (args) => {
-    vim.echo(process.pid);
-});
-
-// vim.addCommand("TSGetCompletions", (args) => {
-//     console.log("TSGETCOMPLETIONS!");
-
-//     var project = tspm.getProjectFromFile(args.currentBuffer);
-
-//     var completions = project.getCompletions(args.currentBuffer, null, args.byte);
-//     if(completions)
-//         console.log("Completion count: " + completions.length);
-//     else
-//         console.log("No completions");
-// });
